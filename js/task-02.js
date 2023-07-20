a758a93434d93b81c5bcfc3d611594ef7923c20a
Vadim CRM <128800269+mr-dorian-black@users.noreply.github.com>
2023-07-20 19:52:09 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const list = document.querySelector('#ingredients');
+const items = (array) => array.forEach(elem => {
+  let item = document.createElement('li');
+  item.textContent = elem;
+  item.className = 'item';
+  list.append(item);
+});
+
+items(ingredients);
