a390ef6ec3aec4b1f002edce122b4fc3fa80a2b9
Vadim CRM <128800269+mr-dorian-black@users.noreply.github.com>
2023-07-21 11:22:10 +0300

    task-06

---
+++ js/task-06.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('#validation-input');
+
+const checkLength = (event) => {
+     let target = event.target;
+     if (target.value.length >= target.dataset.length) {
+          if (!target.classList.contains('valid')) {
+               target.classList.add('valid');
+               if (target.classList.contains('invalid')) target.classList.remove('invalid');
+          }
+     }
+     else {
+          if (!target.classList.contains('invalid')) {
+               target.classList.add('invalid');
+               if (target.classList.contains('valid')) target.classList.remove('valid');
+          }
+     }
+}
+
+input.addEventListener('blur', checkLength);
