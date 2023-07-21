e03a8f39b65d23faa716b993ed07a8932ae0fa91
Vadim CRM <128800269+mr-dorian-black@users.noreply.github.com>
2023-07-21 18:44:26 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('#controls input');
+const buttonCreate = document.querySelector('[data-create]');
+const buttonDestroy = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+const creating = () => {
+  for (let i = 1; i <= input.value; i++) {
+    console.log(i);
+  }
+}
+
+buttonCreate.addEventListener('click', creating)
