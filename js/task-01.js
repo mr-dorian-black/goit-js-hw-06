9332adef7ee39c6a5543e13bd4082831cb0901da
Vadim CRM <128800269+mr-dorian-black@users.noreply.github.com>
2023-07-21 10:18:29 +0300

    task-01-fixed

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,13 @@
 const item = document.querySelector('#categories');
 
 const msg = (elem) => {
-     let text = `Number of categories: ${elem.childElementCount}\n\n${[...elem.children]
+     let text = `Number of categories: ${elem.childElementCount}\n\n` + [...elem.children]
           .map(el => [...el.children]
                .map(ch => {
                     return ch.tagName === "H2" ? `Category: ${ch.textContent}` : `Elements: ${ch.childElementCount}`;
                })
                .join('\n'))
-          .join('\n\n')}`;
+          .join('\n\n');
      return text
 };
 
