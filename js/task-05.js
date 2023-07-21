c119a556cdcef50e8d783925604369d291763049
Vadim CRM <128800269+mr-dorian-black@users.noreply.github.com>
2023-07-21 10:41:15 +0300

    task-05-fixed

--- js/task-05.js
+++ js/task-05.js
@@ -2,12 +2,13 @@ const inputName = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
 const setName = (event) => {
-     nameOutput.textContent = event.target.value;
-}
+     if (event.target.value === '') {
+          nameOutput.textContent = 'Anonymous';
+     }
+     else {
+          nameOutput.textContent = event.target.value;
+     }
 
-const setDefault = (event) => {
-     if (event.target.value === '') nameOutput.textContent = 'Anonymous';
 }
 
 inputName.addEventListener('input', setName);
-inputName.addEventListener('blur', setDefault);
