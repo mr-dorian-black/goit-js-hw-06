527a423bfa7645e6c92e264d1da703e5a3a3bea8
Vadim CRM <128800269+mr-dorian-black@users.noreply.github.com>
2023-07-21 12:48:27 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+const formSender = (event) => {
+     event.preventDefault();
+     const {
+          elements: { email, password }
+     } = event.currentTarget;
+
+     if (email.value === '' || password.value === '') {
+          alert('All fields must be filled!');
+     }
+
+     else {
+          console.log({ email: email.value, password: password.value });
+          form.reset();
+     }
+}
+
+form.addEventListener('submit', formSender);
